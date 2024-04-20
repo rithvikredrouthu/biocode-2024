7e894fcfd80acc9b4795adf261daa79dbdb733ef
rithvikredrouthu <rithvik.redrouthu@gmail.com>
2024-04-20 00:44:58 -0400

    created base app

---
+++ client/app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+export default function Home() {
+  const [message, setMessage] = useState("Loading");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home").then(
+      response => response.json()
+    ).then((data) => {
+      setMessage(data.message);
+    })
+  }, []);
+
+  return (
+    <main>
+      <h1>{ message }</h1>
+    </main>
+  );
+}
